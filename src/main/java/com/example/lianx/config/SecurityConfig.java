a05e60f72511027055299bba13100be7eb37f468
lianx <1119626775@qq.com>
2022-12-16 14:22:53 +0800

    取消ss

--- src/main/java/com/example/lianx/config/SecurityConfig.java
+++ src/main/java/com/example/lianx/config/SecurityConfig.java
@@ -1,39 +1,39 @@
-package com.example.lianx.config;
-
-
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
-import org.springframework.security.web.SecurityFilterChain;
-
-
-@Configuration
-public class SecurityConfig {
-
-    @Bean
-    public WebSecurityCustomizer webSecurityCustomizer() {
-        // 仅仅作为演示
-        return (web) -> web.ignoring().requestMatchers("/resources/**");
-    }
-
-    @Bean
-    SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
-        return http.authorizeHttpRequests(authorize-> {
-                    try {
-                        authorize
-                                // 放行登录接口
-                                .requestMatchers("/login").permitAll()
-                                // 放行资源目录
-                                .requestMatchers("/resources/**").permitAll()
-                                // 其余的都需要权限校验
-                                .anyRequest().authenticated()
-                                // 防跨站请求伪造
-                                .and().csrf(csrf -> csrf.disable());
-                    } catch (Exception e) {
-                        throw new RuntimeException(e);
-                    }
-                }
-        ).build();
-    }
-}
+//package com.example.lianx.config;
+//
+//
+//import org.springframework.context.annotation.Bean;
+//import org.springframework.context.annotation.Configuration;
+//import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+//import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
+//import org.springframework.security.web.SecurityFilterChain;
+//
+//
+//@Configuration
+//public class SecurityConfig {
+//
+//    @Bean
+//    public WebSecurityCustomizer webSecurityCustomizer() {
+//        // 仅仅作为演示
+//        return (web) -> web.ignoring().requestMatchers("/resources/**");
+//    }
+//
+//    @Bean
+//    SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+//        return http.authorizeHttpRequests(authorize-> {
+//                    try {
+//                        authorize
+//                                // 放行登录接口
+//                                .requestMatchers("/login").permitAll()
+//                                // 放行资源目录
+//                                .requestMatchers("/resources/**").permitAll()
+//                                // 其余的都需要权限校验
+//                                .anyRequest().authenticated()
+//                                // 防跨站请求伪造
+//                                .and().csrf(csrf -> csrf.disable());
+//                    } catch (Exception e) {
+//                        throw new RuntimeException(e);
+//                    }
+//                }
+//        ).build();
+//    }
+//}
--- src/main/java/com/example/lianx/controller/UserController.java
+++ src/main/java/com/example/lianx/controller/UserController.java
@@ -14,7 +14,6 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -55,7 +54,6 @@ public class UserController implements CommunityConstant {
     private FollowService followService;
 
     @LoginRequired
-    @PreAuthorize("hasAuthority('user')")
     @RequestMapping(path="/setting",method= RequestMethod.GET)
     public String getSettingPage(){
         return "/site/setting";

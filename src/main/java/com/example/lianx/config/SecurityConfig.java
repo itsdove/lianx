d63fa31a08d07ac67f8e3bf6facacff4f1c164b5
lianx <1119626775@qq.com>
2023-04-13 21:45:33 +0800

    错误处理

--- src/main/java/com/example/lianx/config/SecurityConfig.java
+++ src/main/java/com/example/lianx/config/SecurityConfig.java
@@ -2,6 +2,7 @@
 package com.example.lianx.config;
 
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
@@ -12,6 +13,10 @@ import org.springframework.security.web.SecurityFilterChain;
 @Configuration
 public class SecurityConfig  {
 
+    @Autowired
+    MyAuthenticationEntryPoint myAuthenticationEntryPoint;
+    @Autowired
+    MyAccessDeniedHandler myAccessDeniedHandler;
     @Bean
     public WebSecurityCustomizer webSecurityCustomizer() {
         // 仅仅作为演示
@@ -21,6 +26,7 @@ public class SecurityConfig  {
     @Bean
     SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         http.logout().logoutUrl("/securitylogout");
+
         return http.authorizeHttpRequests(authorize-> {
                     try {
                         authorize
@@ -30,6 +36,10 @@ public class SecurityConfig  {
                                 .requestMatchers("/css/*","/js/*").permitAll()
                                 // 其余的都需要权限校验
                                 .anyRequest().authenticated()
+                                .and()
+                                .exceptionHandling()
+                                .authenticationEntryPoint(myAuthenticationEntryPoint)
+                                .accessDeniedHandler(myAccessDeniedHandler)
                                 // 防跨站请求伪造
                                 .and().csrf(csrf -> csrf.disable());
                     } catch (Exception e) {
